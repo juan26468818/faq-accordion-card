08141b4b6be1775ff968c8e001158c8fe63e014b
juan <juanayala@gmail.com>
2022-10-11 14:02:14 -0300

    fix styles and add functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelectorAll(".button")
+const arrowImg = document.querySelectorAll(".arrow-img")
+const answer = document.querySelectorAll(".answer")
+let activeQuestion = 0
+
+const active = (e) =>{
+    arrowImg[e-1].className += " active-i";
+    button[e-1].className += " active-b";
+    answer[e-1].className += " active-p";
+    if(activeQuestion !== 0) {
+        button[activeQuestion-1].classList.remove("active-b")
+        answer[activeQuestion-1].classList.remove("active-p")
+        arrowImg[activeQuestion-1].classList.remove("active-i")
+    }
+    activeQuestion = e
+}
